ab55f45c4b798c5c0be06085014c769dae936c11
AgneshRamesh04 <74037406+AgneshRamesh04@users.noreply.github.com>
2020-11-06 14:05:51 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -6,9 +6,11 @@ import java.io.ObjectInputStream;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student extends User{
 	
+	private static Scanner scanner = new Scanner(System.in);
 	String student_id;
 	LocalDate accessPeriod_Start;
 	LocalDate accessPeriod_End;
@@ -20,6 +22,59 @@ public class Student extends User{
 		accessPeriod_Start=ld;
 		accessPeriod_End=ld1;
 		//registered_index= rl;
+	}
+	
+	public void printstudentmenu() {
+		do {
+			System.out.println("Please select one of the options below: ");
+			System.out.println("1. Register Course");
+			System.out.println("2. Drop Course");
+			System.out.println("3. Check / Print Courses Registered");
+			System.out.println("4. Check Vacancies Available");
+			System.out.println("5. Change Index Number of Course");
+			System.out.println("6. Swop Index Number with Another Student");
+			System.out.println("7. Change notification mode");
+			System.out.println("8. Change Number or Email");
+			System.out.println("9. Log out");
+			while (!scanner.hasNextInt()) {
+				scanner.next();
+				System.out.println("Please enter valid option:");
+			}
+			int choice = scanner.nextInt();
+
+			switch (choice) {
+			case 1:
+				StudentController.addCourse(student.getMatricNo());
+				break;
+			case 2:
+				StudentController.dropCourse(student.getMatricNo());
+				break;
+			case 3:
+				StudentController.printRegistered(student.getMatricNo());
+				break;
+			case 4:
+				StudentController.checkVacancy();
+				break;
+			case 5:
+				StudentController.changeIndex(student.getMatricNo());
+				break;
+			case 6:
+				StudentController.swopIndex(student.getMatricNo());
+				break;
+			case 7:
+				StudentController.changeNotification(student.getMatricNo());
+				break;
+			case 8:
+				StudentController.changeNumOrEmail(student.getMatricNo());
+				break;
+			case 9:
+				STARSApp.logout();
+				break;
+			default:
+				System.out.println("Please enter valid option:");
+				break;
+			}
+		} while (choice < 0 || choice > 0);
 	}
 	
 	public String getName() {
