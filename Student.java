60d9b6af84a5e128368b12e0a3857982a0db9478
AgneshRamesh04 <74037406+AgneshRamesh04@users.noreply.github.com>
2020-11-06 14:07:59 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -24,58 +24,6 @@ public class Student extends User{
 		//registered_index= rl;
 	}
 	
-	public void printstudentmenu() {
-		do {
-			System.out.println("Please select one of the options below: ");
-			System.out.println("1. Register Course");
-			System.out.println("2. Drop Course");
-			System.out.println("3. Check / Print Courses Registered");
-			System.out.println("4. Check Vacancies Available");
-			System.out.println("5. Change Index Number of Course");
-			System.out.println("6. Swop Index Number with Another Student");
-			System.out.println("7. Change notification mode");
-			System.out.println("8. Change Number or Email");
-			System.out.println("9. Log out");
-			while (!scanner.hasNextInt()) {
-				scanner.next();
-				System.out.println("Please enter valid option:");
-			}
-			int choice = scanner.nextInt();
-
-			switch (choice) {
-			case 1:
-				StudentController.addCourse(student.getMatricNo());
-				break;
-			case 2:
-				StudentController.dropCourse(student.getMatricNo());
-				break;
-			case 3:
-				StudentController.printRegistered(student.getMatricNo());
-				break;
-			case 4:
-				StudentController.checkVacancy();
-				break;
-			case 5:
-				StudentController.changeIndex(student.getMatricNo());
-				break;
-			case 6:
-				StudentController.swopIndex(student.getMatricNo());
-				break;
-			case 7:
-				StudentController.changeNotification(student.getMatricNo());
-				break;
-			case 8:
-				StudentController.changeNumOrEmail(student.getMatricNo());
-				break;
-			case 9:
-				STARSApp.logout();
-				break;
-			default:
-				System.out.println("Please enter valid option:");
-				break;
-			}
-		} while (choice < 0 || choice > 0);
-	}
 	
 	public String getName() {
 		return super.name;
